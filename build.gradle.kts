293ff9d2fa82e0a327efecc6b00f4f5b07054c8e
Baranyai Brigitta <baranyai.brigitta26@gmail.com>
2022-12-04 22:37:08 +0100

    chore: set jvm target

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,21 @@ plugins {
     kotlin("jvm") version "1.7.22"
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
 repositories {
     mavenCentral()
 }
 
 tasks {
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+
     sourceSets {
         main {
             java.srcDirs("src")
